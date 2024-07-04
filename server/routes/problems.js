1b36076db733796fe18290aed45f932dda8562aa
keshri1610 <114894125+keshri1610@users.noreply.github.com>
2024-07-04 23:04:17 +0530

    Update problems.js

--- server/routes/problems.js
+++ server/routes/problems.js
@@ -3,11 +3,18 @@ const router = express.Router();
 const {getProblem,getProblemById,getProblemByText} = require("../controllers/getProblem");
 const {createProblem} = require("../controllers/createProblem");
 const {updateProblem} = require("../controllers/updateProblem");
+const {markProblem, unmarkProblem, getMarkedProblem, storeSolvedQuestion, getSolvedProblem} = require("../controllers/markProblem");
+
 const {deleteProblem} = require("../controllers/deleteProblem");
 const { createContest } = require("../controllers/contestControllers.js/createContest");
 const { getContest, getContestById } = require("../controllers/contestControllers.js/getContest");
 const {signup, login} = require("../controllers/Auth")
 const {checkout,paymentVerification} = require("../controllers/paymentController.js");
+const { getList, getSubmissions } = require("../controllers/Revision.js");
+const { getUserData, manageFriend, getFriendList, addFriend, removeFriend, updateUser } = require("../controllers/UserController.js");
+
+// revisin list
+
 
 router.post("/signup", signup);
 router.post("/login", login);
@@ -15,6 +22,25 @@ router.post("/login", login);
 router.get("/getProblem",getProblem);
 router.get("/getProblemByText",getProblemByText);
 router.get("/getProblem/:id",getProblemById);
+
+router.get("/getMarkedProblem", getMarkedProblem);
+router.put("/storeSolvedQuestion",storeSolvedQuestion);
+router.get("/getSolvedProblem", getSolvedProblem);
+router.put("/markProblem",markProblem);
+router.put("/unmarkProblem", unmarkProblem);
+
+// revision routers
+router.get("/getList",getList);
+router.get("/getSubmissions", getSubmissions);
+
+// users router
+router.get("/getUserData", getUserData);
+router.put("/manageFriend", manageFriend);
+router.put("/addFriend", addFriend);
+router.put("/removeFriend", removeFriend);
+router.get("/getFriendList", getFriendList);
+router.put("/updateUser", updateUser);
+
 router.post("/createProblem",createProblem);
 router.put("/updateProblem/:id",updateProblem);
 router.delete("/deleteProblem/:id",deleteProblem);
